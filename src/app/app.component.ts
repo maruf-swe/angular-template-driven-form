1e22e84f523d564698f7785cfeb13e1db2c7adb9
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2024-01-16 23:53:54 +0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,38 @@
-import { Component, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-template-driven-form';
-  defaultCountry = 'india';
-  defaultGender = 'Male';
-  firstname: any;
-  lastname: any;
-  email: any;
-  gen: any;
-  country: any;
-  hobbies: any;
+  title = 'Reactive Forms';
+  reactiveForm: FormGroup;
 
+  ngOnInit(){
+    this.reactiveForm = new FormGroup({
+      firstname: new FormControl(null, Validators.required),  // for more validation visit 
+      lastname: new FormControl(null, Validators.required),   // https://angular.io/api/forms/Validators
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl('male'),
+      country: new FormControl('bd'),
+      hobbies: new FormControl(null),
+      skills: new FormArray([
+        new FormControl(null),
+        new FormControl(null),
+        new FormControl(null)
+      ])
 
-  gender = [
-    {id: '1', value: 'male'},
-    {id: '2', value: 'Female'},
-    {id: '3', value: 'other'}
-
-  ]
-
-  @ViewChild('myForm') form!: NgForm;
-
-  onSubmit(){
-    console.log(this.form);
-    this.firstname = this.form.value.personDetails.firstname;
-    this.lastname = this.form.value.personDetails.lastname;
-    this.email = this.form.value.personDetails.email;
-    this.gen = this.form.value.gender;
-    this.country = this.form.value.country;
-    this.hobbies = this.form.value.hobbies;
-    this.form.reset();
-
+    });
   }
 
-  setDefaultValues(){
-    // this.form.value.personDetails.firstname="Marufur";
-    // this.form.value.personDetails.lastname="Rahman";
-    // this.form.value.personDetails.email="maruf12gmail.com";
-    this.form.setValue({
-      country: '',
-      gender: '',
-      hobbies: '',
-      personDetails: {
-        firstname:'marufur',
-        lastname:'rahman',
-        email:'maruf12@gmail.com'
-      }
-    })
-
-    this.form.form.patchValue({
-      personDetails: {
-        firstname:'marufur',
-        lastname:'rahman',
-        email:'maruf12@gmail.com'
-      }
-      
-    })
+  onSubmit(){
+    console.log(this.reactiveForm)
 
   }
 }
