ce6d58ddb1d938175b1182928f42a9196599485f
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2024-01-17 22:16:48 +0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,71 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { FormControl } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-template-driven-form';
+  defaultCountry = 'india';
+  defaultGender = 'Male';
+  firstname: any;
+  lastname: any;
+  email: any;
+  gen: any;
+  country: any;
+  hobbies: any;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
-  it(`should have the 'angular-reactive-forms' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-reactive-forms');
-  });
+  gender = [
+    {id: '1', value: 'male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'other'}
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-reactive-forms');
-  });
+  ]
 
-  addSkils(){
-    this.reactiveForm.get('skill;').push(new FormControl(null));
+  @ViewChild('myForm') form!: NgForm;
+
+  onSubmit(){
+    console.log(this.form);
+    this.firstname = this.form.value.personDetails.firstname;
+    this.lastname = this.form.value.personDetails.lastname;
+    this.email = this.form.value.personDetails.email;
+    this.gen = this.form.value.gender;
+    this.country = this.form.value.country;
+    this.hobbies = this.form.value.hobbies;
+    this.form.reset();
+
   }
-});
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstname="Marufur";
+    // this.form.value.personDetails.lastname="Rahman";
+    // this.form.value.personDetails.email="maruf12gmail.com";
+    this.form.setValue({
+      country: '',
+      gender: '',
+      hobbies: '',
+      personDetails: {
+        firstname:'marufur',
+        lastname:'rahman',
+        email:'maruf12@gmail.com'
+      }
+    })
+
+    this.form.form.patchValue({
+      personDetails: {
+        firstname:'marufur',
+        lastname:'rahman',
+        email:'maruf12@gmail.com'
+      }
+      
+    })
+
+  }
+}
